76a265521ea8c02f7c9f0f6b883a78b30d279fe5
Rick <ricksmycodeguy@gmail.com>
2022-01-30 17:00:42 -0600

    Add project files.

---
+++ ClassesIntro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassesIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Car myCar = new Car();
+            Car anotherCar = new Car();
+
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Brake();
+            myCar.Accelerate();
+
+            anotherCar.Brake();
+        }
+    }
+    class Car
+    {
+        private int speed = 0;
+        public void Accelerate()
+        {
+            speed++;
+            Console.WriteLine($"Your're going {speed} Mph ");
+        }
+
+        public void Brake()
+        { 
+            speed--;
+            Console.WriteLine($"You're going {speed} Mph."); 
+        }
+    }
+}
