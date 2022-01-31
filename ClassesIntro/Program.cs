821df4a70d697eb7fcb830c5c18c7436428ac2fe
Rick Mansfield <PEACEFULRICK@msn.com>
2022-01-31 10:13:00 -0600

    Reformats

--- ClassesIntro/Program.cs
+++ ClassesIntro/Program.cs
@@ -2,50 +2,50 @@
 
 namespace ClassesIntro
 {
-    internal class Program
+  internal class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Clear();
-            Car myCar = new Car("Rick's Car");
-            Car anotherCar = new Car("The Batmobile");
+      Console.Clear();
+      Car myCar = new Car("Rick's Car");
+      Car anotherCar = new Car("The Batmobile");
 
-            myCar.Accelerate();
-            myCar.Accelerate();
-            myCar.Accelerate();
-            myCar.Accelerate();
-            myCar.Brake();
-            myCar.Accelerate();
+      myCar.Accelerate();
+      myCar.Accelerate();
+      myCar.Accelerate();
+      myCar.Accelerate();
+      myCar.Brake();
+      myCar.Accelerate();
 
-            anotherCar.Brake();
-        }
+      anotherCar.Brake();
+    }
+  }
+
+  class Car
+  {
+    private int speed = 0;
+    private string name;
+
+    public Car(string carName)
+    {
+      name = carName;
+    }
+    public void Accelerate()
+    {
+
+      speed++;
+      showSpeed();
     }
 
-    class Car
+    public void Brake()
     {
-        private int speed = 0;
-        private string name;
-
-        public Car(string carName)
-        {
-            name = carName;
-        }
-        public void Accelerate()
-        {
-
-            speed++;
-            showSpeed();
-        }
-
-        public void Brake()
-        {
-            speed--;
-            showSpeed();
-        }
-
-        private void showSpeed()
-        {
-            Console.WriteLine($"{name} is going {speed} Mph.");
-        }
+      speed--;
+      showSpeed();
     }
+
+    private void showSpeed()
+    {
+      Console.WriteLine($"{name} is going {speed} Mph.");
+    }
+  }
 }
