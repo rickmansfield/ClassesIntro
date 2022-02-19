8d4328b25d965b4d09946baa34e53ee50d0d9f6f
Rick Mansfield <PEACEFULRICK@msn.com>
2022-02-19 14:42:29 -0600

    Corrects Naming Violation

--- ClassesIntro/Program.cs
+++ ClassesIntro/Program.cs
@@ -2,50 +2,50 @@
 
 namespace ClassesIntro
 {
-  internal class Program
-  {
-    static void Main(string[] args)
+    internal class Program
     {
-      Console.Clear();
-      Car myCar = new Car("Rick's Car");
-      Car anotherCar = new Car("The Batmobile");
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Car myCar = new Car("Rick's Car");
+            Car anotherCar = new Car("The Batmobile");
 
-      myCar.Accelerate();
-      myCar.Accelerate();
-      myCar.Accelerate();
-      myCar.Accelerate();
-      myCar.Brake();
-      myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Brake();
+            myCar.Accelerate();
 
-      anotherCar.Brake();
-    }
-  }
-
-  class Car
-  {
-    private int speed = 0;
-    private string name;
-
-    public Car(string carName)
-    {
-      name = carName;
-    }
-    public void Accelerate()
-    {
-
-      speed++;
-      showSpeed();
+            anotherCar.Brake();
+        }
     }
 
-    public void Brake()
+    class Car
     {
-      speed--;
-      showSpeed();
-    }
+        private int speed = 0;
+        private string name;
 
-    private void showSpeed()
-    {
-      Console.WriteLine($"{name} is going {speed} Mph.");
+        public Car(string carName)
+        {
+            name = carName;
+        }
+        public void Accelerate()
+        {
+
+            speed++;
+            ShowSpeed();
+        }
+
+        public void Brake()
+        {
+            speed--;
+            ShowSpeed();
+        }
+
+        private void ShowSpeed()
+        {
+            Console.WriteLine($"{name} is going {speed} Mph.");
+        }
     }
-  }
 }
